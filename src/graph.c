6acd9086a43d715a917f90347f9d62b23c86d539
leonardo8787 <leocampos8787@gmail.com>
2022-02-13 19:03:48 -0300

    atualização

--- src/graph.c
+++ src/graph.c
@@ -1,47 +1,52 @@
 #include "graph_bfs_dfs.h"
 
 Vertex VertexInitialize(int value) {
-    Vertex v = (Vertex)malloc(sizeof(Vertex));
-    v->value = value;
-    v->prox = NULL;
-    return v;
+  Vertex v = (Vertex)malloc(sizeof(Vertex));
+  v->value = value;
+  return v;
 }
 
 Graph GraphInitialize(int V) {
-    Graph G = (Graph)malloc(sizeof(Graph));
-    G->V = V;
-    G->E = 0;
-    G->adj = (Vertex*)malloc(V * sizeof(Vertex));
-
-    for(int v=0; v < V; v++)
-        G->adj[v] = VertexInitialize(v); // cria apenas os pontos, ainda nao estao conectados
-    
-    return G;
+  Graph G = (Graph)malloc(sizeof(Graph));
+  G->V = V;
+  G->E = 0;
+  G->adj = (Vertex **)malloc(V * sizeof(Vertex *));
+  for (int i = 0; i < G->V; i++) {
+    G->adj[i] = (Vertex *)malloc(G->E * sizeof(Vertex));
+    for (int j = 0; j < 1; j++) {
+      G->adj[i][j] = VertexInitialize(0);
+    }
+  }
+  return G;
 }
 
-void GraphInsertEdge(Graph G, Vertex v1, Vertex v2) { // adicione uma nova ligacao
-    Vertex v = G->adj[v1->value];
-
-    while(v->prox != NULL) { // verifica se ja existe a correlacao
-        if(v->value == v2->value)
-            return;
-        v = v->prox;
+void GraphInsertEdge(Graph G, Vertex v1, Vertex v2) {
+  G->E++;
+  for (int i = 0; i < G->V; i++) {
+    G->adj[i] =
+        (Vertex *)realloc(G->adj[i], G->E * sizeof(Vertex));
+    if (i == v1->value) {
+      G->adj[i][G->E - 1] = VertexInitialize(1);
+    } else if (i == v2->value) {
+      G->adj[i][G->E - 1] = VertexInitialize(-1);
+    } else {
+      G->adj[i][G->E - 1] = VertexInitialize(0);
     }
-
-    v->prox = VertexInitialize(v2->value);
-    G->E++;
+  }
 }
 
 void ImprimeGraph(Graph G) {
-    Vertex aux;
-
-    for(int v=0; v < G->V; v++) {
-        aux = G->adj[v];
-        
-        while(aux != NULL) {
-            printf(" %d -> ", aux->value);
-            aux = aux->prox;
-        }
-        printf("\n");
+  for (int i = 0; i < G->V; i++) {
+    printf("[%2d]-> ", i);
+    for (int j = 0; j < G->E; j++) {
+      if (G->adj[i][j]->value == 1) {
+        printf("[ 1]");
+      } else if (G->adj[i][j]->value == -1) {
+        printf("[-1]");
+      } else {
+        printf("[  ]");
+      }
     }
+    printf("\n");
+  }
 }
--- src/main.c
+++ src/main.c
@@ -2,47 +2,142 @@
 
 int main(){
 
-	int adj[]={};
-	int cont=0;
-	int numero;
+	int op;
 
 	system("clear");
-	printf("\n===========GRAFO============\n\n\n");
+	printf("\n===========GRAFO============\n");
+	printf("Centro Federal de Educação Tecnológica de Minas Gerais\n");
+	printf("Engenharia de Computação\n");
+	printf("Leonardo Campos\n");
+	printf("\n\n\n");
+	printf("Matriz de Incidência\n");
+
+	FILE *file1, *file2, *file3;
 	Graph G = GraphInitialize(10);
+	Vertex v1, v2;
+	v1 = (Vertex)malloc(sizeof(Vertex));
+	v2 = (Vertex)malloc(sizeof(Vertex));
+	char line[100];
+	char *tok;
+	char limite[] = ",\n";
+	clock_t t;
 
-	GraphInsertEdge(G, G->adj[0], G->adj[2]);
-	GraphInsertEdge(G, G->adj[0], G->adj[1]);
-	GraphInsertEdge(G, G->adj[1], G->adj[0]);
-	GraphInsertEdge(G, G->adj[1], G->adj[4]);
-	GraphInsertEdge(G, G->adj[1], G->adj[5]);
-	GraphInsertEdge(G, G->adj[2], G->adj[0]);
-	GraphInsertEdge(G, G->adj[2], G->adj[3]);
-	GraphInsertEdge(G, G->adj[3], G->adj[2]);
-	GraphInsertEdge(G, G->adj[4], G->adj[1]);
-	GraphInsertEdge(G, G->adj[4], G->adj[5]);
-	GraphInsertEdge(G, G->adj[4], G->adj[6]);
-	GraphInsertEdge(G, G->adj[5], G->adj[1]);
-	GraphInsertEdge(G, G->adj[5], G->adj[4]);
-	GraphInsertEdge(G, G->adj[5], G->adj[6]);
-	GraphInsertEdge(G, G->adj[5], G->adj[7]);
-	GraphInsertEdge(G, G->adj[6], G->adj[4]);
-	GraphInsertEdge(G, G->adj[6], G->adj[5]);
-	GraphInsertEdge(G, G->adj[6], G->adj[7]);
-	GraphInsertEdge(G, G->adj[7], G->adj[6]);
-	GraphInsertEdge(G, G->adj[7], G->adj[5]);
+	file1 = fopen("src/pequeno.txt", "r");
+	file2 = fopen("src/medio.txt","r");
+	file3 = fopen("src/grande.txt","r");
 
-	printf("\nLigações\n");
-	ImprimeGraph(G);
+	//GraphInsertEdge(G, G->adj[0][1], G->adj[0][2]);
+	//GraphInsertEdge(G, G->adj[1][0], G->adj[1][4]);
+	//GraphInsertEdge(G, G->adj[1][2], G->adj[2][2]);
+	//GraphInsertEdge(G, G->adj[2][3], G->adj[2][4]);
+	//GraphInsertEdge(G, G->adj[2][5], G->adj[4][3]);
+	//GraphInsertEdge(G, G->adj[3][6], G->adj[4][6]);
+	//GraphInsertEdge(G, G->adj[5][7], G->adj[5][8]);
+	//GraphInsertEdge(G, G->adj[5][2], G->adj[5][1]);
+	//GraphInsertEdge(G, G->adj[8][3], G->adj[6][9]);
+	//GraphInsertEdge(G, G->adj[9][3], G->adj[9][3]);
 
-	printf("\nBFS(Busca em largura)\n");
-	BFS(G, G->adj[0]);
+	do{
+		printf("\nInserções realizadas, escolha uma opção: \n");
+		printf("0 - sair\n");
+		printf("1 - inserção - pequena\n");
+		printf("2 - inserção - média\n");
+		printf("3 - inserção - grande\n");
+		printf("4 - Ligações do grafo\n");
+		printf("5 - Busca em Largura\n");
+		printf("6 - Busca em profundidade\n");
+		printf("7 - limpar a tela\n");
+		scanf("%d", &op);
 
-	printf("\nDFS(Busca em profundidade)\n");
-	DFS(G);
+		switch(op){
+			case 0: 
+				return 0;
+			case 1:
+				if (file1 == NULL) {
+					printf("Erro ao abrir o arquivo!\n");
+					return 0;
+				}
+				printf("Inserindo elementos...\n");
+				t=clock();
+				while ((fgets(line, 100, file1)) != NULL) {
+					tok = strtok(line, limite);
+					int numero = atoi(tok);
+					v1->value = numero;
+					while ((tok = strtok(NULL, limite)) != NULL) {
+					v2->value = atoi(tok);
+					GraphInsertEdge(G, v1, v2);
+					}
+				}
+				t=clock() - t;
+				printf("Tempo de execucao: %lf\n", ((double)t)/((CLOCKS_PER_SEC/1000)));
+				break;
+			case 2:
+				if (file2 == NULL) {
+					printf("Erro ao abrir o arquivo!\n");
+					return 0;
+				}
+				printf("Inserindo elementos...\n");
+				t=clock();
+				while ((fgets(line, 100, file2)) != NULL) {
+					tok = strtok(line, limite);
+					int numero = atoi(tok);
+					v1->value = numero;
+					while ((tok = strtok(NULL, limite)) != NULL) {
+					v2->value = atoi(tok);
+					GraphInsertEdge(G, v1, v2);
+					}
+				}
+				t=clock() - t;
+				printf("Tempo de execucao: %lf\n", ((double)t)/((CLOCKS_PER_SEC/1000)));
+				break;
+			case 3:
+				if (file3 == NULL) {
+					printf("Erro ao abrir o arquivo!\n");
+					return 0;
+				}
+				printf("Inserindo elementos...\n");
+				t=clock();
+				while ((fgets(line, 100, file3)) != NULL) {
+					tok = strtok(line, limite);
+					int numero = atoi(tok);
+					v1->value = numero;
+					while ((tok = strtok(NULL, limite)) != NULL) {
+					v2->value = atoi(tok);
+					GraphInsertEdge(G, v1, v2);
+					}
+				}
+				t=clock() - t;
+				printf("Tempo de execucao: %lf\n", ((double)t)/((CLOCKS_PER_SEC/1000)));
+				break;
+			case 4:
+				printf("\n========Ligações=======\n");
+				t=clock();
+				ImprimeGraph(G);
+				t=clock() - t;
+				printf("Tempo de execucao: %lf\n", ((double)t)/((CLOCKS_PER_SEC/1000)));
+				break;
+			case 5:
+				printf("\nBFS(Busca em largura)===========\n");
+				t=clock();
+				BFS(G, v1);
+				t=clock() - t;
+				printf("Tempo de execucao: %lf\n", ((double)t)/((CLOCKS_PER_SEC/1000)));
+				break;
+			case 6:
+				printf("\nDFS(Busca em profundidade)======\n");
+				t=clock();
+				DFS(G);
+				t=clock() - t;
+				printf("Tempo de execucao: %lf\n", ((double)t)/((CLOCKS_PER_SEC/1000)));
+			case 7:
+				system("clean");
+				break;
+			default:
+				printf("Digite um valor válido!");	
+		}
+	}while(op != 0);
 
-	printf("\n\n==========================================\n");
-	printf("O ");
-
-	return 0;
-
+	fclose(file1);
+	fclose(file2);
+	fclose(file3);
 }
